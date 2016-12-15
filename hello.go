2f334a75c8e7bfaaf7f968436e833eb78f1195be
Nate Nichols <natsamnic@gmail.com>
2016-12-14 16:58:52 -0800

    reverse string

--- hello.go
+++ hello.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+)
 
 func main(){
-  fmt.Printf("Hello, universe.\n")
+  
+  r := []rune("Hello, universe.\n") // make string into an array?
+  x := int len(r)
+  fmt.Printf(string(x))
+  for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+    fmt.Printf(string(r[j]))
+    r[i], r[j] = r[j], r[i]
+  }
+
 }
